bb414b604505700f2a4f816c55341b3cd3689dd7
Francis Ma <fym9823@gmail.com>
2025-12-24 01:23:16 -0800

    initial STS deployment

---
+++ src/arp_sts_keycloak/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+__all__ = ["__version__", "DEFAULT_REALM", "DEFAULT_CLIENTS"]
+
+__version__ = "0.1.0"
+DEFAULT_REALM = "arp-dev"
+DEFAULT_CLIENTS = ("arp-daemon", "arp-runtime", "arp-tool-registry")
